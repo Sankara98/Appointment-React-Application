82c593ae807a6c842de7006a50ebf8f52ece87c4
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2019-07-08 17:26:04 -0400

    Fixed App directory location in index js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import AppointmentComponent from './Components/Appointment/AppointmentComponent'
-import '../App.css'
+import './App.css'
 class App extends Component  {
 
   constructor() {
